90404ed5b1ace74f7950da1aa33ae65c091bdb68
Rishika Singh <147064849+rishika2426@users.noreply.github.com>
2025-09-07 16:29:10 +0530

    Update binaryS.c

--- binaryS.c
+++ binaryS.c
@@ -5,7 +5,7 @@ void input(int arr[],int size)
 	for(int i=0;i<size;i++)
 	{
 		printf("[%d]: ",i);
-		scanf("%d",&array[i]);
+		scanf("%d",&arr[i]);
 	}
 }
 
@@ -18,23 +18,31 @@ int binaryS(int arr[], int size, int element)
 	while(f<=l)
 	{
 		if(f<arr[mid])
+		{
 			l=mid-1;
+		}
 		else if(f>arr[mid])
+		{	
 			f=mid+1;
+		}
 		else
+		{
 			return mid;
-	}		
+		}
+	}
+	return 0;		
 }
 
 int main()
 {
-	int size, element;
+	int size,i, element;
 	printf("Enter the size of array: ");
 	scanf("%d",&size);
 	int arr[size];
+	input(arr,size);
 	printf("Enter the element to be searched: ");
 	scanf("%d",&element);
-	int i=binaryS(arr,size,element);
+	i=binaryS(arr,size,element);
 	printf("%d found at %d position",element,i+1);
 	return 0;
 }
